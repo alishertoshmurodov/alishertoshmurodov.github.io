0af0529d258af5e8df4c86a92710e0aefb92f022
Alisher <toshmurodov2001@mail.ru>
2022-12-15 12:52:32 +0500

    v 1.0

--- resources/script.js
+++ resources/script.js
@@ -1,1 +1,22 @@
+const projectEls = document.querySelectorAll('.projects__container__item');
 
+projectEls.forEach(element => {
+    element.addEventListener('mouseover', function (e) {
+        element.classList.add('scale-up');
+    });
+    element.addEventListener('mouseout', function (e) {
+        element.classList.remove('scale-up');
+    });
+});
+
+
+const contactLinkEls = document.querySelectorAll('.contacts__container a');
+
+contactLinkEls.forEach(element => {
+    element.addEventListener('mouseover', function (e) {
+        element.classList.add('scale-center');
+    });
+    element.addEventListener('mouseout', function (e) {
+        element.classList.remove('scale-center');
+    });
+});
