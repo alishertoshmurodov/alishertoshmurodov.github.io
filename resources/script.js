f553bf4e3cb6799f4ccf355d9104a478efe8b2b0
Alisher <toshmurodov2001@mail.ru>
2022-12-18 14:07:15 +0500

    minor bug fixes

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,18 @@
 const projectEls = document.querySelectorAll('.projects__container__item');
 
+
 projectEls.forEach(element => {
-    element.addEventListener('mouseover', function (e) {
-        element.classList.add('scale-up');
-    });
-    element.addEventListener('mouseout', function (e) {
-        element.classList.remove('scale-up');
-    });
+    const media = window.matchMedia('(max-width: 425px)');
+    if (media.matches) {
+        removeEventListener('mouseover', projectEls.forEach);
+    } else {
+        element.addEventListener('mouseover', function (e) {
+            element.classList.add('scale-up');
+        });
+        element.addEventListener('mouseout', function (e) {
+            element.classList.remove('scale-up');
+        });
+    }
 });
 
 
@@ -20,3 +26,7 @@ contactLinkEls.forEach(element => {
         element.classList.remove('scale-center');
     });
 });
+
+
+
+
